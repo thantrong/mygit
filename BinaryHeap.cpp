6b8d8ca4e2210fa7797668538c3a6841b9f9e493
thantrong <thantrongsv0@gmail.com>
2024-08-01 21:32:40 +0700

    thêm chú thích

--- BinaryHeap.cpp
+++ BinaryHeap.cpp
@@ -7,14 +7,14 @@
 
 
 using namespace std;
-
+/// In Binary Heap
 void BinaryHeap::display() {
     for(int h : this->heap){
         cout<<h<<" ";
     }
     cout<<endl;
 }
-
+/// Lấy ra phân tử lớn nhất trong Heap
 int BinaryHeap::get_max() {
     if(this->heap.empty()){
 
@@ -23,19 +23,19 @@ int BinaryHeap::get_max() {
     }
     return this->heap[0];
 }
-
+/// Vị trí của cha
 int BinaryHeap::parten(int index) {
     return (index)>>1;
 }
-
+/// Vị trí con trái
 int BinaryHeap::left_child(int index) {
     return index<<1;
 }
-
+/// Vị trí con phải
 int BinaryHeap::right_child(int index) {
     return (index<<1)+1;
 }
-
+/// Kiểm trả index nhận vào có lớn hơn cha không nếu có thì swap vị trí với nhau và tiếp tục kiểm tra
 void BinaryHeap::heap_up(int index) {
 
     if(this->heap[parten(index)]<this->heap[index]){
@@ -45,7 +45,7 @@ void BinaryHeap::heap_up(int index) {
         heap_up(parten(index));
     }
 }
-
+/// Kiểm tra đứa được đưa lên làm cha có nhỏ hơn 2 đứa con trái và phải không nếu có thì swap và tiếp tục kiểm tra xuống
 void BinaryHeap::heap_dow(int index) {
     int l_child = left_child(index);
     int r_child = right_child(index);
@@ -64,13 +64,14 @@ void BinaryHeap::heap_dow(int index) {
         heap_dow(head);
     }
 }
-
+///Nhập phần tử vào Heap
 void BinaryHeap::insert(int element) {
 
     this->heap.push_back(element);
 
     heap_up(this->heap.size()-1);
 }
+/// Xóa phần tử lớn nhất
 void BinaryHeap::remove_max() {
     if(this->heap.empty()){
     cout<<"Heap Emty !!!";
